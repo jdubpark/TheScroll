c0953da6e80118087a47379b57f1abcd77464439
jwparktom <jwparktom@gmail.com>
2019-10-31 17:47:09 -0400

    Update 7

---
+++ public/article.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="parkjongwon.com" />
+    <title>The Scroll - DA Student Newspaper</title>
+    <!-- <link rel="canonical" href="https://thescroll.com/" /> -->
+    <link rel="stylesheet" href="./lib/style/dist/normalize.min.css" />
+    <link rel="stylesheet" href="./lib/style/dist/universal.min.css<?php echo "?v=".filemtime("./lib/style/dist/universal.min.css"); ?>" />
+    <link rel="stylesheet" href="./lib/style/dist/article.min.css<?php echo "?v=".filemtime("./lib/style/dist/article.min.css"); ?>" />
+    <link rel="stylesheet" href="./lib/style/dist/article-format.min.css<?php echo "?v=".filemtime("./lib/style/dist/article-format.min.css"); ?>" />
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:600,600i|Spectral:400,400i,700,700i|Libre+Baskerville:400,400i&display=swap" rel="stylesheet">
+    <!-- Gentium+Basic:400 -->
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+  </head>
+
+  <body>
+
+    <div id="layout">
+      <div id="header" class="layout-container layout-header">
+        <div id="site-nav" class="header-parent">
+          <div id="header-mini" class="header-row header-mini amplified">
+            <div class="header-container header-container-mini">
+              <div class="header-wrapper header-wrapper-top">
+                <div class="header-top-col mini header-top-items header-top-col-left">
+                  <div class="header-top-item mini header-top-menu-trigger">
+                    <span class="one"></span>
+                    <span class="two"></span>
+                    <span class="three"></span>
+                  </div>
+                  <div class="header-top-item mini header-top-search-trigger">
+                    <img src="./lib/svg/icon-search.svg" title="Search" />
+                  </div>
+                </div>
+                <div class="header-top-col mini header-top-col-center">
+                  <a class="header-top-logo mini" href="./" title="Go to Deerfield Scroll homepage"></a>
+                </div>
+                <div class="header-top-col mini header-top-col-right">
+                  <div class="header-mini-actions">
+                    <div class="header-mini-action-btn header-mini-action-subscribe"><a>Subscribe</a></div>
+                    <div class="header-mini-action-btn"><a href="./login.php">Login</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="header-nav" class="header-row header-nav standalone">
+            <div class="header-nav-container">
+              <div class="header-nav-list">
+                <div class="header-nav-item"><a href="./">Home</a></div>
+                <div class="header-nav-item"><a href="./">News</a></div>
+                <div class="header-nav-item"><a href="./">Features</a></div>
+                <div class="header-nav-item"><a href="./">Opinion</a></div>
+                <div class="header-nav-item"><a href="./">Editorial</a></div>
+                <div class="header-nav-item"><a href="./">Arts</a></div>
+                <div class="header-nav-item"><a href="./">Sports</a></div>
+                <div class="header-nav-item"><a href="./">Buzz</a></div>
+              </div>
+              <!-- <div class="header-nav-vd"></div> -->
+              <div class="header-nav-list">
+                <div class="header-nav-item"><a href="./">Archives</a></div>
+                <div class="header-nav-item"><a href="./">About</a></div>
+              </div>
+              <div class="header-nav-list">
+                <div class="header-nav-item"><a href="./">Vol. XCVII</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="content" class="layout-container layout-content layout-article">
+        <div id="article-viewer" class="arf arf-fluid">
+          <div class="arf-container">
+            <div class="arv-header arf-header">
+              <div class="arf-block">
+                <div class="arf-hd-title">Deerfield Academy Installs All-Gender Bathroom Signage</div>
+                <div class="arf-hd-summary">In recent weeks, all-gender restroom signage has been placed on six single-stall restrooms across campus.</div>
+              </div>
+              <div class="arf-hd-cover">
+                <div class="arf-hd-cover-image"><img src="./lib/img/random-img.jpg" /></div>
+                <div class="arf-hd-cover-caption">According to Molly Fischer ’20, a leader of the Gender and Sexuality Alliance, the goal of the bathrooms is “to show the LGBTQ+ community at Deerfield, specifically trans- and non-binary people, that there is a safe place on campus for them to use the bathroom without having to mis-gender themselves.”</div>
+              </div>
+            </div>
+            <div class="arv-body arf-body">
+              <div class="arf-meta arf-mt">
+                <div class="arf-mt-row">
+                  <div class="arf-mt-item arf-mt-author">By Camille Glatt '21 <span>Staff Writer</span></div>
+                  <div class="arf-mt-item arf-mt-published">Published <span>May 29, 2019</span></div>
+                </div>
+                <div class="arf-mt-row">
+                </div>
+              </div>
+              <div class="arf-content arf-ct">
+                <p>
+                  In recent weeks, all-gender restroom signage has been placed on six single-stall restrooms across campus.
+                </p>
+                <p>
+                  These new signs mark success for students advocating for gender inclusivity at Deerfield. This change is credited to leaders of the Gender and Sexuality Alliance, who have been pushing for the installation of these signs for months.
+                </p>
+                <p>
+                  According to Molly Fischer ’20, a leader of the Gender and Sexuality Alliance, the goal of the bathrooms is “to show the LGBTQ+ community at Deerfield, specifically trans- and non-binary people, that there is a safe place on campus for them to use the bathroom without having to mis-gender themselves.”
+                </p>
+                <p>
+                  While some have appreciated and recognized these changes, others have also expressed concern about the relative lack of recognition that the additions have garnered on campus.
+                </p>
+                <p>
+                  Both students and faculty members have noted that, given a lack of knowledge or promotion on the initiative, the Deerfield community remains unable to appreciate the change as an important aspect of the school’s culture.
+                </p>
+                <p>
+                  Lexi Roadside ‘21 commented, “These bathrooms have the potential to promote inclusion on campus, which is great, but many people [still] don’t know about the bathrooms or don’t believe that they are incredibly important, which is a problem.”
+                </p>
+                <p>
+                  The GSA’s main objectives are to address these problems and spread further awareness. Fischer said, “[The goal was] to make trans and non-binary people at Deerfield more visible, to initiate more conversations about gender on Deerfield’s campus, and to show the LGBTQ+ community at Deerfield that the administration supports them.”
+                </p>
+                <p>
+                  This change entailed a lengthy and meticulous process. Fischer explained, “I had quite a few conversations with Ms. Creagh to speak about the purpose, location, design, and terminology of the all-gender bathroom signs. I also had to present several modified sign designs to the senior staff and administrators at Deerfield.”
+                </p>
+                <p>
+                  There is currently a restroom with all-gender signage installed in the Field House, Dining Hall, Kendall, Armes, MSB, and Health Center. Given that there were no bathrooms redesigned or built for this project, the changes entailed remained relatively simple. In fact, the only part of the bathroom that were changed were the signs.
+                </p>
+                <p>
+                  Voicing the significance of this change, Ethics teacher Benjamin Grimm explained, “The bathrooms were already all-gender; the official use of that language shows that we are acknowledging that the binary gender system does not include everyone equally. By adding signage, we’re recognizing the need to think inclusively about our physical spaces and helping to make that resource more visible and accessible to all people.”
+                </p>
+                <p>
+                  Although this change in signs is a notable step forward for the realm of LGBTQ+ awareness and acceptance on campus, there is still a lot of work to be done. As Mr. Grimm noted, “A sign for an all-gender restroom will not automatically make Deerfield more inclusive.”
+                </p>
+                <p>
+                  Looking into the future, the GSA plans to continue promoting awareness, using all-gender bathrooms as a starting point for further work.
+                </p>
+                <p>
+                  As Fischer shared, “I think [all-gender bathrooms are] a start to making more people feel included at Deerfield. There are other parts of Deerfield’s campus and policies that I would like to see changed to make them more inclusive and accepting of the LGBTQ+ community at Deerfield.”
+                </p>
+                <p>
+                  The step to change these signs has marked an important movement; Deerfield is attempting to become a safer and more welcoming environment for those of all gender expressions.
+                </p>
+                <p>
+                  Mr. Grimm said, “[the signage] is a physical manifestation of our commitment to inclusivity and a visible marker to everyone that Deerfield is a school that values and serves people of all gender identities and expressions.”
+                </p>
+              </div>
+            </div>
+            <div class="arv-more arf-short">
+              <div class="arv-mr-header arv-mr-hd">
+                <div class="arv-mr-hd-title">More to read</div>
+              </div>
+              <div class="arv-mr-content arv-mr-ct">
+                <div class="arv-mr-article">
+                  <div class="arv-mr-ar-img" style="background-image:url('./lib/img/random-img.jpg');"></div>
+                  <div class="arv-mr-ar-section">Opinion</div>
+                  <div class="arv-mr-ar-headline">Celebrating Margarita Curtis, 55th HOS</div>
+                  <div class="arv-mr-ar-published">May 29</div>
+                </div>
+                <div class="arv-mr-article">
+                  <div class="arv-mr-ar-img" style="background-image:url('./lib/img/random-img.jpg');"></div>
+                  <div class="arv-mr-ar-section">Opinion</div>
+                  <div class="arv-mr-ar-headline">Celebrating Margarita Curtis, 55th HOS</div>
+                  <div class="arv-mr-ar-published">May 29</div>
+                </div>
+                <div class="arv-mr-article">
+                  <div class="arv-mr-ar-img" style="background-image:url('./lib/img/random-img.jpg');"></div>
+                  <div class="arv-mr-ar-section">Opinion</div>
+                  <div class="arv-mr-ar-headline">Celebrating Margarita Curtis, 55th HOS</div>
+                  <div class="arv-mr-ar-published">May 29</div>
+                </div>
+                <div class="arv-mr-article">
+                  <div class="arv-mr-ar-img" style="background-image:url('./lib/img/random-img.jpg');"></div>
+                  <div class="arv-mr-ar-section">Opinion</div>
+                  <div class="arv-mr-ar-headline">Celebrating Margarita Curtis, 55th HOS</div>
+                  <div class="arv-mr-ar-published">May 29</div>
+                </div>
+                <div class="arv-mr-article">
+                  <div class="arv-mr-ar-img" style="background-image:url('./lib/img/random-img.jpg');"></div>
+                  <div class="arv-mr-ar-section">Opinion</div>
+                  <div class="arv-mr-ar-headline">Celebrating Margarita Curtis, 55th HOS</div>
+                  <div class="arv-mr-ar-published">May 29</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer" class="layout-container layout-home">
+
+      </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+	</body>
+</html>
